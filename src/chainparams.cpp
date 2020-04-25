44eb18772c030ab5f4fb2ddc5cfb6d405eb4be77
NodeCoin-Dev <antoinebrulenew@gmail.com>
2020-04-25 13:00:58 -0400

    Changed the genesis key

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -78,7 +78,7 @@ static CBlock CreateDevNetGenesisBlock(const uint256 &prevBlockHash, const std::
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "Decentralization is key - 4/16/20";
-    const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
+    const CScript genesisOutputScript = CScript() << ParseHex("042a755d96e0d772a8687d51059705ff47f8056114079e9a7e32d4d9d780ca7014f4794b171f392f05a74947c4a2465271dd8a9d444eba8c2f381f0e4926d42dd0") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
