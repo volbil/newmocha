809c90fbb6bc72364af2ed0ba6f97abac9d23e22
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-04-26 21:35:06 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -77,7 +77,7 @@ static CBlock CreateDevNetGenesisBlock(const uint256 &prevBlockHash, const std::
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "Decentralization is key - 4/16/20";
-    const CScript genesisOutputScript = CScript() << ParseHex("042a755d96e0d772a8687d51059705ff47f8056114079e9a7e32d4d9d780ca7014f4794b171f392f05a74947c4a2465271dd8a9d444eba8c2f381f0e4926d42dd0") << OP_CHECKSIG;
+    const CScript genesisOutputScript = CScript() << ParseHex("042a755d96e0d772a8687d51059705ff47f8056114079e9a7e32d4d9d780ca7014f4794b171f392f05a74947c4a2465271dd8a9d444eba8c") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
